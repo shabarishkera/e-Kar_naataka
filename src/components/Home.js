5ab1e3fb9c825ebcf34c5b95898465b7754d4dc5
shabarish <shabarishkera@gmail.com>
2023-08-30 16:00:22 +0530

    added features

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,11 +1,14 @@
 import Navbar from './Navbar';
-import Hero from './Hero'
+import Hero from './Hero';
+import Features from './Features'
 export default function  Home(argument) {
 	
 	return (
 		<>
 		
 		<Hero/>
+		<h1 className="bg-warning my-2 mx-2">Our Features </h1>
+		<Features/>
 
 </>
 
