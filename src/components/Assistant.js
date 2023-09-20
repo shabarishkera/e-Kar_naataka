c9f240f1c6a42d605494b2d6d8b5373903272eda
shabarish <shabarishkera@gmail.com>
2023-09-20 18:55:22 +0530

    modfied login page and added assistant support

--- src/components/Assistant.js
+++ src/components/Assistant.js
@@ -1,7 +1,11 @@
 export default function Assistant(props)
 { return (<>
 
-	<div className="assist-wrap"></div>
+	<div className="assist-wrap">
+	<i className="bi bi-volume-down-fill asist-icon"></i>
+	<i className="bi bi-pause asist-icon"></i>
+	<i className="bi bi-stop-fill asist-icon"></i>
+	</div>
 		</>)
 
 }
--- src/components/Login.js
+++ src/components/Login.js
@@ -1,4 +1,5 @@
 import {Link,useNavigate}from 'react-router-dom'
+import  styles from '../loginsignup.module.css'
 import {useRef} from 'react'
 export default function Login() {
 const email=useRef()
@@ -33,34 +34,30 @@ nav("/")
 	return (<>
 
 
-    <div class="container mt-5">
-        <div class="row justify-content-center">
-            <div class="col-md-6">
-                <div class="card">
-                    <div class="card-header">
-                        <h3>Login</h3>
-                    </div>
-                    <div class="card-body">
-                        <div>
-                            <div class="mb-3">
-                                <label htmlFor="username" class="form-label">Email</label>
-                                <input type="text" class="form-control" ref={email} id="username" name="username" required/>
-                            </div>
-                            <div class="mb-3">
-                                <label htmlFor="password" class="form-label">Password</label>
-                                <input type="password" class="form-control" ref={password} id="password" name="password" required/>
-                            </div>
-                            <div class="d-grid mb-3">
-                                <button type="submit" class="btn btn-primary" onClick={handleLogin}>Login</button>
-                            </div>
-                        </div>
-                        <p class="mb-0">Don't have an account? <Link to="/signup">Sign Up</Link></p>
-                    </div>
-                </div>
-            </div>
+  <section className={styles.section}>
+   <div className={styles.login_box}>
+    <form onSubmit={(e)=>{e.preventDefault()}} action="" >
+        <h2 className={styles.h2}>Login</h2>
+        <div className={styles.input_box}>
+            <span className={styles.icon}><ion-icon name="mail"></ion-icon></span>
+            <input type="email" required placeholder="Email" ref={email}/>
+          
         </div>
-    </div>
-
+        <div className={styles.input_box}>
+            <span className={styles.icon}><ion-icon name="lock-closed-outline"></ion-icon><ion-icon name="lock"></ion-icon></span>
+            <input type="password" required placeholder="Password" ref={password}/>
+            
+            
+        </div>
+       
+        <button className={styles.button} type="submit" onClick={handleLogin}>Login</button>
+        <div className={styles.register_link}>
+            <p className={styles.p}>Don't have an account?<Link to="/signup">SignUp</Link></p>
+        </div>
+    </form>
+   </div>
+</section>
+   
 
 		</>)
 
