92eaf853bc86c44723cee0a88f0808c7e187b873
shabarish <shabarishkera@gmail.com>
2023-11-24 19:54:14 +0530

    added confiriming before logout

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,14 +1,16 @@
 import {Link ,useNavigate} from 'react-router-dom'
 import {Helmet} from 'react-helmet';
 import {useState } from 'react'
+import ConfirmBox from './ConfirmBox'
 export default function Navbar()
 {
   const [isLogged,setLogged]=useState(localStorage.getItem('token')?true:false)
-
+const [modalvisible,setmodalvisible]=useState(false);
 const nav=useNavigate();
 const handleLogout=()=>{
 localStorage.removeItem('token');
 setLogged(false)
+setmodalvisible(false)
 window.location.reload();
 
 }
@@ -21,9 +23,11 @@ return (<><nav className="navbar navbar-expand-lg navbar-dark bg-dark z-3">
  <i className="bi bi-boxes text-light h1 mx-3"></i>
   <Link  className="navbar-brand  " to="#">ಇ-Karನಾಟಕ</Link>
 
+{/* use an comformation box that gets a state and a function  */}
   <button className="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNavDropdown" aria-controls="navbarNavDropdown" aria-expanded="false" aria-label="Toggle navigation">
     <span className="navbar-toggler-icon"></span>
   </button>
+ <ConfirmBox modalvisible={modalvisible}title={'Logout'} message={'Are you sure to logout ?'} setmodelvisible={setmodalvisible} handlerfunction={handleLogout}/>
   <div className="collapse navbar-collapse" id="navbarNavDropdown">
     <ul className="navbar-nav">
       <li className="nav-item active">
@@ -43,7 +47,7 @@ return (<><nav className="navbar navbar-expand-lg navbar-dark bg-dark z-3">
         <Link  className="nav-link" to="/myLearning">My Learning</Link>
       </li>
      <li className="nav-item">
-     <button className="btn btn-outline-warning my-2 my-sm-0 mx-2 log-in-out-btn " type="submit" onClick={localStorage.getItem('token')?handleLogout:handleLogin}>{localStorage.getItem('token')?'Logout':'Login'}</button>
+     <button className="btn btn-outline-warning my-2 my-sm-0 mx-2 log-in-out-btn " type="submit" onClick={localStorage.getItem('token')?()=>{setmodalvisible(true)}:handleLogin}>{localStorage.getItem('token')?'Logout':'Login'}</button>
      </li>
 
      
