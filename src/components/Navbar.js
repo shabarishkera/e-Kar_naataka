6277a9817a000f1ae3a9aea7f6966aae32229e0a
shabarish <shabarishkera@gmail.com>
2023-12-14 19:41:20 +0530

    added store and given default entry for navigation bar colring

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,9 +1,11 @@
 import {Link ,useNavigate} from 'react-router-dom'
 import {Helmet} from 'react-helmet';
-import {useState } from 'react'
+import {useContext, useState } from 'react'
 import ConfirmBox from './ConfirmBox'
+import { context } from '../store/Store';
 export default function Navbar()
 {
+  const {navbarColor,setNavbarColor,navtextColor}=useContext(context)
   const [isLogged,setLogged]=useState(localStorage.getItem('token')?true:false)
 const [modalvisible,setmodalvisible]=useState(false);
 const nav=useNavigate();
@@ -19,9 +21,9 @@ const handleLogin=async()=>{
  nav("/login");
 
   }
-return (<><nav className="navbar navbar-expand-lg navbar-dark bg-dark z-3">
- <i className="bi bi-boxes text-light h1 mx-3"></i>
-  <Link  className="navbar-brand  " to="#">ಇ-Karನಾಟಕ</Link>
+return (<><nav className={`navbar navbar-expand-lg navbar-dark bg-${navbarColor} z-3`}>
+ <i className={`bi bi-boxes text-${navtextColor} h1 mx-3`}></i>
+  <Link  className={`navbar-brand text-${navtextColor}  `} to="#">ಇ-Karನಾಟಕ</Link>
 
 {/* use an comformation box that gets a state and a function  */}
   <button className="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNavDropdown" aria-controls="navbarNavDropdown" aria-expanded="false" aria-label="Toggle navigation">
@@ -30,24 +32,24 @@ return (<><nav className="navbar navbar-expand-lg navbar-dark bg-dark z-3">
  <ConfirmBox modalvisible={modalvisible}title={'Logout'} message={'Are you sure to logout ?'} setmodelvisible={setmodalvisible} handlerfunction={handleLogout}/>
   <div className="collapse navbar-collapse" id="navbarNavDropdown">
     <ul className="navbar-nav">
-      <li className="nav-item active">
-        <Link  className="nav-link" to="/">Home</Link>
+      <li className="nav-item  active">
+        <Link  className={`nav-link text-${navtextColor}`} to="/">Home</Link>
       </li>
       <li className="nav-item">
-        <Link  className="nav-link" to="/history">History</Link>
+        <Link  className={`nav-link text-${navtextColor}`} to="/history">History</Link>
       </li>
       <li className="nav-item">
-        <Link  className="nav-link" to="/search">Search</Link>
+        <Link  className={`nav-link text-${navtextColor}`} to="/search">Search</Link>
       </li>
 
       <li className="nav-item">
-        <Link  className="nav-link" to="/explore">Explore</Link>
+        <Link  className={`nav-link text-${navtextColor}`} to="/explore">Explore</Link>
       </li>
       <li className="nav-item">
-        <Link  className="nav-link" to="/myLearning">My Learning</Link>
+        <Link  className={`nav-link text-${navtextColor}`} to="/myLearning">My Learning</Link>
       </li>
     { isLogged && <li className="nav-item">
-        <Link  className="nav-link" to="/profile">Profile</Link>
+        <Link  className={`nav-link text-${navtextColor}`} to="/profile">Profile</Link>
       </li>}
      <li className="nav-item">
      <button className="btn btn-outline-warning my-2 my-sm-0 mx-2 log-in-out-btn " type="submit" onClick={localStorage.getItem('token')?()=>{setmodalvisible(true)}:handleLogin}>{localStorage.getItem('token')?'Logout':'Login'}</button>
--- src/index.js
+++ src/index.js
@@ -5,11 +5,13 @@ import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
-
+import Store from './store/Store';
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
+   <Store>
     <App />
+    </Store>
   </React.StrictMode>
 );
 
